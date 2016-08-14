3b3414927d8c70157400975081253e376e54334e
Lrss <Soerensen007@gmail.com>
2016-08-14 14:31:48 +0200

    Added milestone Todo's for the project.

--- src/ml/lrss/Main.java
+++ src/ml/lrss/Main.java
@@ -1,15 +1,36 @@
 package ml.lrss;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class Main {
 
     public static void main(String[] args) {
         if (args.length >= 1) {
-            if (args[0].equals("calc"))
-                Convert.CliConvert();
-            else
-                System.out.println(args[0] + " Is not a valid argument");
+            switch (args[0]) {
+                case "calc":
+                    //representation calculator
+                    Convert.CliConvert();
+                    break;
+                case "asm":
+                    //Todo: assembly translator.
+                    throw new NotImplementedException();
+                case "gate":
+                    //Might be out of scope.
+                    throw new NotImplementedException();
+                case "pipe":
+                    //Might be out of scope.
+                    throw new NotImplementedException();
+                case "cache":
+                    //Todo: Cache accessing simplifier
+                    throw new NotImplementedException();
+                default:
+                    System.out.println(
+                            "\"" + args[0] + "\" is not a valid argument!");
+                    break;
+            }
         } else {
-            System.out.println("You need to type an arguments");
+            System.out.println(
+                    "You need to type an arguments (calc, asm, gate, pipe or cache).");
         }
     }
 }
