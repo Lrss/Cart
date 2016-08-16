345a9cb7e21753dd1ff72f9517e3d7705f03044f
Lars Sørensen <Lsar13@student.aau.dk>
2016-08-16 19:17:52 +0200

    Streamlined compilation process.

--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,18 @@
 javac Convert.java Cart.java
 echo Main-Class: Cart > MANIFEST.MF
 jar -cvmf MANIFEST.MF Cart.jar Convert.class Cart.class
-cat stub.sh Cart.jar > Cart.run && chmod +x Cart.run 
+echo '#!/bin/sh
+MYSELF=`which "$0" 2>/dev/null`
+[ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
+java=java
+if test -n "$JAVA_HOME"; then
+    java="$JAVA_HOME/bin/java"
+fi
+exec "$java" $java_args -jar $MYSELF "$@"
+exit 1' > stub.sh
+#chmod +x stub.sh
+cat stub.sh Cart.jar > cart && chmod +x cart
+rm *.class
+rm *.jar
+rm *.MF
+rm stub.sh
